3a3f86df76941a767400e595eba9a3e15512b4f9
Justin Goats <justingoats@yahoo.com>
2021-06-21 03:28:38 -0700

    commit to master

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const app = express();
+let express = require("express");
+let app = express();
+let nodemailer = require("nodemailer");
+let PORT = process.env.PORT || 3000;
 
-let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: process.env.USERNAME,
-        pass: process.env.PASSWORD,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    },
-});
-
-transporter.verify((err, success) => {
-    err
-        ? console.log(err)
-        : console.log(`=== Server is ready to take messages: ${success} ===`);
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.post("/send", (req, res) => {
-    let mailOptions = {
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: process.env.USERNAME,
+            pass: process.env.PASSWORD,
+            clientId: process.env.OAUTH_CLIENTID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        }
+    });
+
+    var mailOptions = {
         from: req.body.email,
         to: process.env.USERNAME,
         subject: `message from ${req.body.firstname} ${req.body.lastname} from ${req.body.email}`,
         text: req.body.message
     };
 
-    transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.send(error);
         } else {
-            console.log("Email sent successfully");
-            res.json({ status: "Email sent" });
+            var string = encodeURIComponent('200');
+            //res.json({ status: "Email sent" });
+            return res.redirect("localhost:3000?response=" + string);
         }
     });
 });
 
-
-const port = 3001;
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`)
 });
